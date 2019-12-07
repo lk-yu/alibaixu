3a67f3dc1f3e22a8d404e602b1832c8829478708
Alan <1597498028@qq.com>
2019-12-07 11:10:17 +0800

    第二次提交

--- routes/actions/post/find.js
+++ routes/actions/post/find.js
@@ -13,15 +13,15 @@ module.exports = async (req, res) => {
 	// 查询条件
 	let condition = {};
 	// 分类条件
-	if (req.query.category != undefined) {
+	if (req.query.category != undefined && req.query.category !== '') {
 		condition.category = req.query.category;
 	}
 	// 状态条件
-	if (req.query.state != undefined) {
+	if (req.query.state != undefined && req.query.state !== '') {
 		condition.state = req.query.state;
 	}
 	// 查询用户信息
-	const posts = await pagination(Post).page(page).size(10).display(5).find(condition).populate('author', '-password').populate('category').select('-content -meta').exec();
+	const posts = await pagination(Post).page(page).size(5).display(5).find(condition).populate('author', '-password').populate('category').select('-content -meta').exec();
 	// 响应
 	res.send(posts);
 }
