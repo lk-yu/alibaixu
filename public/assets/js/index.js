02e5544c1e3b2302fd6f35dce78b434033120c26
Alan <1597498028@qq.com>
2019-12-08 19:44:47 +0800

    仪表盘代码优化

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,26 +1,28 @@
-//文章总数量获取数据
-$.ajax({
-    type:'get',
-    url:'/posts/count',
-    success:function(res){
-        // console.log(res);
-        $('#post').html(`<strong>${res.postCount}</strong>篇文章（<strong>${res.draftCount}</strong>篇草稿）`)
-    }
-});
-//分类总数量获取数据
-$.ajax({
-    type:'get',
-    url:'/categories/count',
-    success:function(res){
-        // console.log(res);
-        $('#category').html(`<strong>${res.categoryCount}</strong>个分类`)
-    }
-});//评论总数量获取数据
-$.ajax({
-    type:'get',
-    url:'/comments/count',
-    success:function(res){
-        // console.log(res);
-        $('#comment').html(`<strong>${res.commentCount}</strong>条评论`)
-    }
-});
+//并行多发请求
+$.when(
+    //文章总数量获取数据
+    $.ajax({
+        type: 'get',
+        url: '/posts/count'
+    }),
+    // 分类总数量获取数据
+    $.ajax({
+        type: 'get',
+        url: '/categories/count'
+    }),
+    // 评论总数量获取数据
+    $.ajax({
+        type: 'get',
+        url: '/comments/count'
+    })
+
+
+).done(function (r1, r2, r3) {
+    console.log(r1, r2, r3);
+    let html = template('dashboardTpl',{
+        r1:r1[0],
+        r2:r2[0],
+        r3:r3[0]
+    })
+    $('#dashboardBox').html(html)
+})
